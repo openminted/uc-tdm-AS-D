60a88a4445cfab073b55e641895b9adcd8c78852
ldeleger <ldeleger@gmail.com>
2017-07-25 15:20:46 +0200

    Updated indexer and postagging

--- plans/run_WoS_abstracts_batch.sh
+++ plans/run_WoS_abstracts_batch.sh
@@ -10,6 +10,6 @@ RAM=30g
 . $CONFIG_FILE
 
 # run the pipeline
-$ALVISDIR/bin/alvisnlp -J "-Xmx30g" -verbose -entity inputfile $CORPUSDIR/$CORPUSNAME-part$batch -inputDir $PLANDIR -inputDir $SOFTWAREDIR -outputDir $OUTPUTDIR/part$batch -entity outdir $OUTPUTDIR/part$batch -entity ontology $ONTOLOGY_NAME plans/tag_and_index_WoS_abstracts.plan
+$ALVISDIR/bin/alvisnlp -J "-Xmx30g" -verbose -entity inputfile $CORPUSDIR/$batch/batch.wos -inputDir $PLANDIR -inputDir $SOFTWAREDIR -outputDir $OUTPUTDIR/$batch -entity outdir $OUTPUTDIR/$batch -entity ontology $ONTOLOGY_NAME plans/tag_and_index_WoS_abstracts.plan
 #$ALVISDIR/bin/alvisnlp -J "-Xmx30g" -verbose -entity inputfile $CORPUSDIR/$CORPUSNAME-part$batch -inputDir $PLANDIR -inputDir $SOFTWAREDIR -outputDir $OUTPUTDIR/part$batch -entity outdir $OUTPUTDIR/part$batch -entity ontology $ONTOLOGY_NAME plans/test.plan
 
