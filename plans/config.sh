5ec742bdcc5b79eb012d1717db4d5ae33ed0e534
ldeleger <ldeleger@gmail.com>
2018-04-27 15:07:04 +0200

    Updated config file

--- plans/config.sh
+++ plans/config.sh
@@ -1,7 +1,5 @@
 
 export ALVISDIR=/path/to/alvisnlp/ # install dir of AlvisNLP 
 export CORPUSDIR=/path/to/corpus # location of the corpus
-export PLANDIR=/path/to/plans/dir/ # full path to the directory containing the plans 
-export SOFTWAREDIR=/path/to/tools/dir/ # directory where Treetagger and Yatea are installed
-export ONTOLOGYNAME=WheatPhenotypeOntology-v2.0a # name of the ontology used for mapping wheat phenotypes
+export SOFTWAREDIR=/path/to/tools/dir/ # directory where tools are installed
 export OUTPUTDIR=/path/to/output/directory # where all output files and folders will be created
