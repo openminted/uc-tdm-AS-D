d015a39c0d287c37dda60825bccde6da5bfa51ee
ldeleger <ldeleger@gmail.com>
2018-04-27 15:10:01 +0200

    Updated command line to run workflow

--- plans/run_WoS_abstracts_batch.sh
+++ plans/run_WoS_abstracts_batch.sh
@@ -3,13 +3,9 @@
 batch=$1 # get the number of the batch to process
 CONFIG_FILE=$2 # get the configuration file to use
 
-CORPUSNAME=WoS-ref_gene_marker_wheat-janv-17
-RAM=30g
-
 # import variables
 . $CONFIG_FILE
 
 # run the pipeline
-$ALVISDIR/bin/alvisnlp -J "-Xmx30g" -verbose -entity inputfile $CORPUSDIR/$batch/batch.wos -inputDir $PLANDIR -inputDir $SOFTWAREDIR -outputDir $OUTPUTDIR/$batch -entity outdir $OUTPUTDIR/$batch -entity ontology $ONTOLOGY_NAME plans/tag_and_index_WoS_abstracts.plan
-#$ALVISDIR/bin/alvisnlp -J "-Xmx30g" -verbose -entity inputfile $CORPUSDIR/$CORPUSNAME-part$batch -inputDir $PLANDIR -inputDir $SOFTWAREDIR -outputDir $OUTPUTDIR/part$batch -entity outdir $OUTPUTDIR/part$batch -entity ontology $ONTOLOGY_NAME plans/test.plan
+$ALVISDIR/bin/alvisnlp -J-Xmx30g -verbose -alias input $CORPUSDIR/$batch/batch.wos -inputDir $PWD -inputDir $SOFTWAREDIR -outputDir $OUTPUTDIR/$batch plans/tag_and_index_WoS_abstracts.plan
 
